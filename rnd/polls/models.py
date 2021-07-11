740364ab0558d523dbaa77b9301d6633db4647aa
Alexander Gomeniuk <gomeniuk.aa@gmail.com>
2021-07-12 01:13:31 +1000

    Setup models

--- rnd/polls/models.py
+++ rnd/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    queestion_text = models.CharField(max_length=256)
+    publication_date = models.DateTimeField(verbose_name='date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=128)
+    votes = models.IntegerField(default=0)
